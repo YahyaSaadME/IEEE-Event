e7bfe3a05725f0a014ffa741a816c9acdd412183
YahyaSaadME <yahyasaad.me@gmail.com>
2024-08-20 23:33:29 +0530

    Title update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="bg-transparent text-white/95 bg-cover bg-top h-screen flex flex-col justify-center">
       <div className="w-full max-w-7xl mx-auto flex flex-col justify-center px-4 xl:px-0 mt-24">
         <h1 className="font-display text-6xl max-md:text-5xl tracking-wider text-center md:text-start">
-          Hello There!
+          Hi👋, There!
         </h1>
         <p className="font-light mt-4 w-full md:max-w-[50%] sm:text-xs md:text-md  text-center md:text-start">
           Join us on 22nd August for an insightful webinar hosted by the IEEE
